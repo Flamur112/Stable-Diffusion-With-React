d21eaf9024ffb275a65cf3c4d5a14e1423452a63
nona <nona@nonas-Air.home>
2024-07-16 18:22:42 +0200

    Controller and Logic added + fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, HttpCode, Redirect, Param, HostParam, Body, Patch, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, Redirect, Param, HostParam, Body, Patch, Delete, Query, ParseIntPipe } from '@nestjs/common';
 import { UsersService }  from './users.service';
 
 @Controller('users') // /users
@@ -6,29 +6,29 @@ export class UsersController {
 
     constructor(private readonly usersService: UsersService) {} // curly braced there to not see all red shit
 
-    @Get() // GET /users r /users?role=value&age=42
-    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){ // findAll method. role?: , the ? stands for optional so that if a parameter is declared without '?' like 'role: string' then it means that when you call the function or method you must provide a 'role'.
+    @Get() // GET /users r /users?role=value&age=42 // FILTERATION LOGIC
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){ // role?: is getting assigned the variable of ThisWillShowInURL which basically means it is getting assigned the key-value pairs of it and making it optional to have values after it. In the findAll method, role?: ,the ? stands for optional so that if a parameter is declared without '?' like 'role: string' then it means that when you call the function or method you must provide a 'role'.
         return this.usersService.findAll(role)
     }
 
     @Get(':id') // GET /users/:id
-    findOne(@Param('id') id: string) {
-        return  this.usersService.findOne(+id) // the + before id is called a unary plus which transforms the type into integer cause in the findOne method from the users.service.ts logic we defined it as an integer but iin the controller method we defined it as a function. And if we use the + before the variable and its already a variable it will have no effect.
+    findOne(@Param('id', ParseIntPipe) id: number) { // after @Param('id') decorator we declare id variable and the id will be a string.
+        return  this.usersService.findOne(id) // the + before id is called a unary plus which transforms the type into integer cause in the findOne method from the users.service.ts logic we defined it as an integer but iin the controller method we defined it as a function. And if we use the + before the variable and its already a variable it will have no effect.
     }
 
-    @Post() // POST /users
-    create(@Body() user: { name: string, email: string,  role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
-        return this.usersService.create(user)
+    @Post() // Allows for this function to now be able to send data to server
+    create(@Body() user: { name: string, email: string,  role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) { // The @Body says that anything after that decorator @Body() is what is going to be sent to the server
+        return this.usersService.create(user) // turns data from client side to server side
     }
 
     @Patch(':id') // PATCH /users/:id
-    update(@Param('id')id: string, @Body() userUpdate: { name?: string, email?: string,  role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }){ // userUpdate is a variable name the : {} gives userUpdate a type object
-        return this.usersService.update(+id, userUpdate)
+    update(@Param('id', ParseIntPipe)id: number, @Body() userUpdate: { name?: string, email?: string,  role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }){ // userUpdate is a variable name the : {} gives userUpdate a type object
+        return this.usersService.update(id, userUpdate)
     }
 
     @Delete(':id') // DELETE /users/:id
-    delete(@Param('id') id: string){
-        return this.usersService.delete(+id) // the + before the id stands for unary plus. It transforms the type to a number as its a string in this methid.
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.usersService.delete(id) // the + before the id stands for unary plus. It transforms the type to a number as its a string in this methid.
     }
 
     // @Patch(':id') // PATCH /users/:id
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {
+export class UsersService { //creates a class called UsersService and allows for it to be exported aka used in another file.
     private users = [
         {
           "id": 1,
@@ -36,29 +36,29 @@ export class UsersService {
         }
       ]
 
-      findAll(role?: 'INTERN' | 'ENGINEER'| 'ADMIN'){
-        if (role) {
-            return this.users.filter(user => user.role === role)
+      findAll(role?: 'INTERN' | 'ENGINEER'| 'ADMIN'){ // declaring the findALL method and then the role?: is declaring the role variable and assigning the values represented afterwards.
+        if (role) { // if role is truthy (if it has a value)
+            return this.users.filter(user => user.role === role) // return this.database.filter out. The user => is calling the user method and then for every user with role property equal to the role. If role is === to the role of the users 'role' property then return the users whole array.
         }
-        return this.users
+        return this.users // return the entire array of users stored in the UsersService class.
       }
 
       findOne(id: number){
-        const user = this.users.find(user => user.id === id)
+        const user = this.users.find(user => user.id === id) // finds and locates the first element of the user.id that is equal to the number.
         return user
       }
 
-      create(user: {name: string, email: string,  role: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
-        const usersByHighestId = [...this.users].sort((a,b) => b.id - a.id)
-        const newUser = {
+      create(user: { name: string, email: string,  role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) { // we give the user variable the object literal of {name: string, email: string,  role: 'INTERN' | 'ENGINEER' | 'ADMIN'}
+        const usersByHighestId = [...this.users].sort((a,b) => b.id - a.id) // making sure and setting usersByHighestId[0] will have the highest 'id'.
+        const newUser = { // Increments the id of the highest user by 1 to create a new unique id for newUser
             id: usersByHighestId[0].id + 1,
             ...user 
         }
-        this.users.push(newUser)
+        this.users.push(newUser) // add the new array to the end of the user.id 
         return newUser
       }
 
-      update(id: number, updatedUser: {name?: string, email?: string,  role?: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
+      update( id: number, updatedUser: { name?: string, email?: string,  role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }){
         this.users = this.users.map(user => {
             if ( user.id === id ) {
                 return { ...user, ...updatedUser }
