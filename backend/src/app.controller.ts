5724c4c6a2633c7ff70a78ff7362c2fb7f3fbf1c
Flamur112 <flamurmatoshi1@hotmail.com>
2024-08-05 22:37:14 +0200

    setup

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -14,19 +14,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-
-
-
-
-  // @Get('home')
-  // getGello(@Res() res: Response): void {
-  //   const filePath = join('/Users/nona/Sites/www/pages', 'home.html'); // Path to your HTML file
-  //   res.sendFile(filePath);
-  // }
-  // // @Get('home')
-  // getGello(@Res() res: Response): void {
-  //   const filePath = join(__dirname, '..', 'public', 'home.html');
-  //   res.sendFile(filePath);
-  // }
-
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'; // Optional for managing environment variables
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
 import { StableDifusionIntegrationModule } from './modules/stable-difusion-integration/stable-difusion-integration.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // Optional for environment variables
-    UsersModule, StableDifusionIntegrationModule,
+    StableDifusionIntegrationModule,
   ],
   controllers: [AppController],
   providers: [AppService,]
