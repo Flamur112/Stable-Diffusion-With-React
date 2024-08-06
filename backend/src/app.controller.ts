bb2d649dead0cd6d83f95e4298895e8bb31dba09
nona <nona@nonas-Air.home>
2024-08-06 18:29:46 +0200

    React

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 
-@Controller()
+@Controller() // used to allow for us to use routes within this function
 export class AppController {
   constructor(
     private readonly appService: AppService,
