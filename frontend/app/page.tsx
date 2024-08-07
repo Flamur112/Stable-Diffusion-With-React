e15848cf53b8ad598b6b29807f9ae12ddb814f4c
Flamur <34803365+Flamur112@users.noreply.github.com>
2024-08-07 10:38:10 -1100

    api setup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,36 +1,12 @@
-import Image from "next/image";
-import { StableWebsite } from "./components/StableWebsite";
+import React from 'react';
+import { StableWebsite } from './components/StableWebsite';
 
-interface Image {
-  url: string;
-  prompt: String;
-}
-
-const fetchImageData = async (prompt: string) => {
-  const res = await fetch('https://dummyjson.com/RESOURCE/?limit=10&skip=5&select=key1,key2,key3', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ prompt }),
-  });
-  if (!res.ok) {
-    throw new Error('Failed to fetch image data');
-  }
-  const data = await res.json();
-  return data;
-};
 
 export default function Home() {
-
-
-
-
   return (
     <main>
-      <h1>landing Page</h1>
+      <h1>Landing Page</h1>
       <StableWebsite />
     </main>
   );
 }
-
