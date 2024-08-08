af0776cdbdcccbafe5fcc356d06c6703876edabc
nona <nona@nonas-Air.home>
2024-08-08 14:26:39 +0200

    React Fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,6 +7,11 @@ export default function Home() {
     <main>
       <h1>Landing Page</h1>
       <StableWebsite />
+
+      <div className="container">
+  <h1 className="text-3xl font-bold">Welcome to My Website</h1>
+  <p className="mt-4">This is a paragraph inside a responsive container.</p>
+</div>
     </main>
   );
-}
+};
